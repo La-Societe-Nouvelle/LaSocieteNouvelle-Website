d90b45923d7c8fa577a54826f538f3e8566db2d9
lyderichti59 <lyderic.dutillieux@origenial.fr>
2021-03-16 14:56:29 +0100

    Changed title !
    Signed-off-by: lyderichti59 <lyderic.dutillieux@origenial.fr>

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  let testons=true;
   return (
     <div className={styles.container}>
       <Head>
@@ -11,9 +12,8 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://la-societe-nouvelle.fr">La Société Nouvelle</a>
         </h1>
-
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
