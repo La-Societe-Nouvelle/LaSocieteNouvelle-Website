23ae712856946b8aa6527768ae411b5638f2fc8b
SylvainH-LSN <sylvain.humiliere@la-societe-nouvelle.fr>
2021-04-09 07:47:40 +0000

    Add link for GitHub "new issue"  in the footer

--- pages/footer.js
+++ pages/footer.js
@@ -9,6 +9,7 @@ export default class Footer extends React.Component {
         <div id="links">
             <a className="link-footer" href="https://www.linkedin.com/company/la-societe-nouvelle/" target="_blanck">Page LinkedIn</a>
             <a className="link-footer" href="https://github.com/SylvainH-LSN/LaSocieteNouvelle-Website" target="_blanck">Repertoire GitHub</a>
+            <a className="link-footer" href="https://github.com/SylvainH-LSN/LaSocieteNouvelle-Website/issues" target="_blanck">Signaler un problème</a>
             <a className="link-footer" href="/company-data?siren=889182770">Empreinte Sociétale</a>
             <a className="link-footer" href="/mentions-legales">Mentions légales</a>
             <a className="link-footer" href="/contact">Contact</a>
