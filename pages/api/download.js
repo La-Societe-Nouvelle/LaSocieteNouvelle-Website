66cef5f942afef765ea12778b4bbd33f4323d24f
Sylvain HUMILIERE <sylvain.humiliere@la-societe-nouvelle.fr>
2021-04-28 11:43:03 +0200

    Update Downloader

--- pages/api/download.js
+++ pages/api/download.js
@@ -5,11 +5,9 @@ export default async (req,res) => {
     const filePath = req.query.filename;     
     // filename only
     const fileName = filePath.substring(filePath.lastIndexOf("/") + 1);
-    // set header
-    let test = fs.readFileSync('./public/documentation-api.md');
-    
+    // set header    
     res.setHeader("content-disposition", "attachment; filename="+fileName);
-    //res.download(filePath);
+    
     var filestream = fs.createReadStream(filePath);
     filestream.pipe(res);
 
