8e2f318ed8bc8007955454a07878df7763054947
Sylvain HUMILIERE <sylvain.humiliere@la-societe-nouvelle.fr>
2021-04-28 10:16:27 +0200

    Correction path

--- pages/api/download.js
+++ pages/api/download.js
@@ -3,13 +3,10 @@ var fs = require('fs');
 export default async (req,res) => {
 
     const filePath = req.query.filename;     
-    console.log("checka");
     // filename only
     const fileName = filePath.substring(filePath.lastIndexOf("/") + 1);
-    console.log("check0");
     // set header
     res.setHeader("content-disposition", "attachment; filename="+fileName);
-    console.log("check");
     //res.download(filePath);
     var filestream = fs.createReadStream(filePath);
     filestream.pipe(res);
