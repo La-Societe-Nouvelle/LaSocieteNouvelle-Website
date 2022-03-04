d533da535c496a4ccbc4cd729c095859c9add893
Laura Roost <laura.roost@lasocietenouvelle.org>
2022-03-04 11:25:16 +0100

    Ajout du lien twitter

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,8 +9,9 @@ export default class Footer extends React.Component {
         <div id="links">
             <a className="link-footer" href="/mentions-legales">Mentions légales</a>
             <a className="link-footer" href="/company-data?siren=889182770">Empreinte Sociétale</a>
-            <a className="link-footer" href="https://github.com/SylvainH-LSN/LaSocieteNouvelle-Website" target="_blanck">Repertoire GitHub</a>
-            <a className="link-footer" href="https://www.linkedin.com/company/la-societe-nouvelle/" target="_blanck">Page LinkedIn</a>
+            <a className="link-footer" href="https://github.com/SylvainH-LSN/LaSocieteNouvelle-Website" target="_blank">Repertoire GitHub</a>
+            <a className="link-footer" href="https://www.linkedin.com/company/la-societe-nouvelle/" target="_blank">LinkedIn</a>
+            <a className="link-footer" href="https://twitter.com/SHumiliere" target="_blank">Twitter</a>
             {/*<a className="link-footer" href="/contact">Contact</a>*/}
         </div>
 
