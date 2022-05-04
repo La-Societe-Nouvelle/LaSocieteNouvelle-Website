05686c8bd757f2c4106f6d221efd16cd3d2d8ce1
LauraR-LSN <laura.roost@lasocietenouvelle.org>
2022-05-04 12:29:19 +0200

    buttons in header added

--- src/components/header.js
+++ src/components/header.js
@@ -17,14 +17,22 @@ export default class Header extends React.Component {
          <img id="logo-header" className="fluid float-start" src="/images/logo-lsn-header.png" alt="logo-lsn-header"/>
 
           <h1 className="slogan">
-              Mesurer, Informer pour une <span className="underline"> économie durable </span>
+              Mesurer, Informer pour une économie durable
           </h1>
            </div>
          </Col> 
          <Col>
+         <div className="text-end pt-2">
            <h2>
               Initiative OpenData - OpenSource
              </h2> 
+            <a className="btn btn-cta ms-2" href="http://metriz.lasocietenouvelle.org" target="_blank">
+               Mesurer mes impacts  
+            </a> 
+            <a className="btn btn-publish" href="/publication">
+              Publier mes données  
+            </a> 
+         </div>
          </Col>
         </Row>
         </Container>
