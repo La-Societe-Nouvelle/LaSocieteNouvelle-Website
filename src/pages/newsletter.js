d948c63b6637dc77de08bdc10d900a5957154655
Laura Roost <laura.roost@lasocietenouvelle.org>
2022-09-06 18:04:28 +0200

    new newsletter subscription form

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -2,7 +2,7 @@ import { Helmet } from "react-helmet";
 
 // Components
 
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Image } from "react-bootstrap";
 import PageHeader from "../components/PageHeader";
 
 export default function Newsletter() {
@@ -12,25 +12,20 @@ export default function Newsletter() {
         <title>La société Nouvelle | Newsletter</title>
       </Helmet>
       <PageHeader title="Inscription à notre Newsletter" path="newsletter" />
-      <section>
         <Container>
-          <h2>Abonnez-vous à lettre d'information de la Société Nouvelle</h2>
-          <p>
-            Vous pouvez vous abonner gratuitement à notre lettre d'information.
-            Vous recevrez un courriel sur les dernières actualités ou les
-            dernières missions de la Société Nouvelle. Votre adresse e-mail est
-            uniquement utilisée pour vous envoyer la lettre d'information. Vous
-            pouvez à tout moment vous désinscrire en utilisant le lien de
-            désabonnement en bas de toutes nos lettres d'informations.
-          </p>
+          <div>
           <Row>
-            <Col md={{ span: 8, offset: 2 }}>
-            <iframe class="mj-w-res-iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://app.mailjet.com/widget/iframe/7EBb/NVE" width="100%"></iframe>
-            <script type="text/javascript" src="https://app.mailjet.com/statics/js/iframeResizer.min.js"></script>
+            <Col md={8}>
+            <iframe width="100%" height="650" src="https://a2dec458.sibforms.com/serve/MUIEAAM5_uzMBmA2XvCDybECJekJcvgKpEir_BfDxg8oWxjYYknTx-mcfgLipT7aEac7_wSnElKSBRobR0YHyWCT-Qjab9XMnNIFoCl5JGy9YXOkYq9Xhnl3KSDGd1PwXpKLM-Hk8MOSvxTk_E2jTz3NgHiEXci2FOQaI-zrHgA3StaW2sbBsYkZC_r5BV4znKfobC4aJ9IbO9a3" frameborder="0" scrolling="auto" allowfullscreen ></iframe>
+ 
+            </Col>
+            <Col>
+            <Image className="mt-5" fluid src="images/Newsletter-cuate.svg"></Image>
             </Col>
           </Row>
+          </div>
+        
         </Container>
-      </section>
     </>
   );
 }
