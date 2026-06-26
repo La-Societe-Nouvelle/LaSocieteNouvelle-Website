4c1b984638a90c2b8d527598749eef24ae3cc49d
Sylvain HUMILIERE <sylvain.humiliere@la-societe-nouvelle.fr>
2026-06-26 12:56:09 +0200

    masque bandeau partenariat 2026

--- app/page.js
+++ app/page.js
@@ -24,7 +24,7 @@ export const metadata = {
 export default function Home() {
   return (
     <>
-      <PartnershipBanner />
+      {/* <PartnershipBanner /> */}
       <HeroSection />
       <KeyFigures />
       <IndicsSection />
